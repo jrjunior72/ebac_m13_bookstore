84d83206db9956f6e8df00cf24624123214d25c7
jrjunior72 <jrjunior72@hotmail.com>
2025-08-12 23:55:13 -0300

    Exercício M17: autenticação via Token aplicada ao OrderViewSet

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -1,12 +1,9 @@
 import json
 
 from django.urls import reverse
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 from rest_framework.views import status
 
-
-from order.factories import UserFactory
 from product.factories import CategoryFactory, ProductFactory
 from product.models import Product
 
@@ -15,32 +12,25 @@ class TestProductViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
-        self.user = UserFactory()
-        token = Token.objects.create(user=self.user)  # added
-        token.save()  # added
-
         self.product = ProductFactory(
             title="pro controller",
             price=200.00,
         )
 
     def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username)  # added
-        self.client.credentials(
-            HTTP_AUTHORIZATION="Token " + token.key)  # added
         response = self.client.get(
-            reverse("product-list", kwargs={"version": "v1"}))
-
+            reverse("product-list", kwargs={"version": "v1"})
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        product_data = json.loads(response.content)
 
-        self.assertEqual(product_data['results'][0]["title"], self.product.title)
-        self.assertEqual(float(product_data['results'][0]["price"]), self.product.price)
-        self.assertEqual(product_data['results'][0]["active"], self.product.active)
+        product_data = response.data["results"]
+
+        self.assertEqual(product_data[0]["title"], self.product.title)
+        self.assertEqual(float(product_data[0]["price"]), self.product.price)
+        self.assertEqual(product_data[0]["active"], self.product.active)
 
     def test_create_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+
         category = CategoryFactory()
         data = json.dumps({
             "title": "notebook", 
